740884b325dc7440406785909f0d3d7f2f08d012
jeddy3 <jeddy3@users.noreply.github.com>
2018-08-02 10:25:28 +0100

    Use current whitelists
    To make the config more easily consumerable.

--- index.js
+++ index.js
@@ -8,40 +8,6 @@ module.exports = {
     "stylelint-selector-bem-pattern"
   ],
   rules: {
-    "plugin/color-hex-whitelist": [
-      "#6aa300",
-      "#cc0070",
-      "#0099a9",
-      "#6e2066",
-      "#e86c00",
-      "#c30000",
-      "#1d1d1d",
-      "#fff"
-    ],
-    "plugin/font-font-size-whitelist": ["/var\\(--type-/"],
-    "plugin/function-arguments-whitelist": {
-      shade: ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%"],
-      tint: ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%"]
-    },
-    "plugin/property-value-rem-multiples": [
-      {
-        "/^border": 4,
-        "/box-shadow/": 4,
-        font: 4,
-        "grid-gap": 4,
-        "line-height": 4,
-        "/^margin/": 4,
-        "/^padding/": 4
-      },
-      { ignoreNumbers: ["0.125", "0.25", "0.5", "0.75"] }
-    ],
-    "order/order": ["custom-properties", "declarations"],
-    "order/properties-alphabetical-order": [true, { severity: "warning" }],
-    "plugin/selector-bem-pattern": [
-      {
-        preset: "suit"
-      }
-    ],
     "at-rule-empty-line-before": [
       "always",
       {
@@ -52,7 +18,7 @@ module.exports = {
     ],
     "at-rule-no-unknown": null,
     "at-rule-no-vendor-prefix": true,
-    "at-rule-whitelist": [],
+    "at-rule-whitelist": ["font-face", "import", "media"],
     "color-hex-length": "short",
     "color-no-hex": true,
     "custom-property-empty-line-before": [
@@ -73,25 +39,125 @@ module.exports = {
     ],
     "declaration-no-important": true,
     "declaration-property-value-whitelist": {
-      border: ["none"],
-      "font-size": ["/^var\\(--type-/"]
+      border: ["none"]
     },
     "declaration-property-unit-whitelist": {
-      outline: ["px", "rem"],
+      outline: ["%", "px", "rem"],
       "/^font/": ["rem"],
-      "/^margin/": ["rem"],
+      "/^margin/": ["rem", "vw"],
       "/^padding/": ["rem"]
     },
     "font-family-no-missing-generic-family-keyword": null,
     "function-url-no-scheme-relative": true,
     "function-url-scheme-whitelist": [],
-    "function-whitelist": [],
+    "function-whitelist": [
+      "calc",
+      "color-mod",
+      "minmax",
+      "repeat",
+      "shade",
+      "tint",
+      "url",
+      "var"
+    ],
     "media-feature-name-no-vendor-prefix": true,
-    "media-feature-name-whitelist": [],
+    "media-feature-name-whitelist": ["min-width"],
     "number-max-precision": 4,
+    "order/order": ["custom-properties", "declarations"],
+    "order/properties-alphabetical-order": [true, { severity: "warning" }],
+    "plugin/color-hex-whitelist": [
+      "#6aa300",
+      "#cc0070",
+      "#0099a9",
+      "#6e2066",
+      "#e86c00",
+      "#c30000",
+      "#1d1d1d",
+      "#fff"
+    ],
+    "plugin/font-size-whitelist": ["/var\\(--type-/"],
+    "plugin/function-arguments-whitelist": {
+      shade: ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%"],
+      tint: ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%"]
+    },
+    "plugin/property-value-rem-multiples": [
+      {
+        "/^border": 4,
+        "/box-shadow/": 4,
+        font: 4,
+        "grid-gap": 4,
+        "line-height": 4,
+        "/^margin/": 4,
+        "/^padding/": 4
+      },
+      { ignoreNumbers: ["0.125", "0.25", "0.5", "0.75"] }
+    ],
+    "plugin/selector-bem-pattern": [
+      {
+        preset: "suit"
+      },
+      { severity: "warning" }
+    ],
     "property-no-unknown": null,
     "property-no-vendor-prefix": true,
-    "property-whitelist": [],
+    "property-whitelist": [
+      "align-items",
+      "appearance",
+      "background-color",
+      "background-image",
+      "background-size",
+      "background",
+      "border-radius",
+      "border",
+      "bottom",
+      "box-shadow",
+      "box-sizing",
+      "color",
+      "content",
+      "display",
+      "flex-basis",
+      "flex-direction",
+      "flex-wrap",
+      "flex",
+      "font-family",
+      "font-size",
+      "font-style",
+      "font-variant-numeric",
+      "font-weight",
+      "font",
+      "grid-column",
+      "grid-gap",
+      "grid-row",
+      "grid-template-columns",
+      "grid-template-rows",
+      "grid",
+      "height",
+      "justify-content",
+      "left",
+      "line-height",
+      "list-style",
+      "margin-bottom",
+      "margin-left",
+      "margin-right",
+      "margin-top",
+      "margin",
+      "max-width",
+      "min-width",
+      "padding-left",
+      "padding-top",
+      "padding",
+      "position",
+      "resize",
+      "right",
+      "src",
+      "text-align",
+      "text-decoration",
+      "text-indent",
+      "top",
+      "white-space",
+      "width",
+      "z-index"
+    ],
     "rule-empty-line-before": [
       "always",
       {
@@ -105,11 +171,19 @@ module.exports = {
     "selector-max-id": 0,
     "selector-max-type": 0,
     "selector-max-universal": 0,
+    "selector-nested-pattern": "^&",
     "selector-no-vendor-prefix": true,
     "selector-pseudo-class-no-unknown": null,
-    "selector-pseudo-class-whitelist": [],
+    "selector-pseudo-class-whitelist": [
+      "active",
+      "first-child",
+      "focus",
+      "hover",
+      "not",
+      "root"
+    ],
     "unit-no-unknown": null,
-    "unit-whitelist": [],
+    "unit-whitelist": ["%", "fr", "px", "rem", "vw"],
     "value-no-vendor-prefix": true
   }
 };
